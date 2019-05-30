07a3f185fa6d4e92991c2816a7c86c982554b9ba
bcmmbaga <bethuelmbaga12@gmail.com>
2019-05-30 13:28:58 +0300

    add env build_branch

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const app = express()
 
 
 const commitRef = process.env.APP_COMMIT_REF || 'N/A'
+const commitBranch = process.env.APP_BUILD_BRANCH || 'N/A'
 const buildDate = process.env.APP_BUILD_DATE || new Date().toISOString()
 
 app.get('/', (req, res) => {
     const message = myLib.helloDeploy()
-    const text = `${message}! We're at commit ${commitRef} which was built at ${buildDate}`
+    const text = `${message}! We're at branch ${commitBranch} commit ${commitRef} which was built at ${buildDate}`
     res.send(text)
 })
 
